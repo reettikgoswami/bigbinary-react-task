1ccc67891f6b272264555638757e842c4d7940d5
reettik97 <reettikgoswami1997@gmail.com>
2020-07-18 19:08:31 +0530

    Move filter inputs to a separate component

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,38 +1,8 @@
 import React, { Component } from "react";
-import {
-  Header,
-  Menu,
-  Checkbox,
-  Dropdown,
-  Label,
-  Form,
-  Pagination,
-} from "semantic-ui-react";
+import { Pagination } from "semantic-ui-react";
 
-import MessageBox from "./MessageBox";
-import Modal from "./Modal";
-import DatePicker from "react-datepicker";
 import LaunchList from "./LaunchList";
-const tagOptions = [
-  {
-    key: "All Launches",
-    text: "All ",
-    value: "All",
-    label: { color: "blue", empty: true, circular: true },
-  },
-  {
-    key: "Successfull",
-    text: "Successfull",
-    value: "Successfull",
-    label: { color: "green", empty: true, circular: true },
-  },
-  {
-    key: "Failed",
-    text: "Failed",
-    value: "Failed",
-    label: { color: "red", empty: true, circular: true },
-  },
-];
+import FilterInput from "./FilterInput";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -42,137 +12,12 @@ class Dashboard extends Component {
     };
   }
 
-  modalClose = () => {
-    this.setState({ modalIsOpen: false });
-  };
-  massageBoxStyle = {
-    overlay: {
-      position: "fixed",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: "rgba(255, 255, 255, 0.75)",
-      zIndex: 1000,
-    },
-    content: {
-      position: "absolute",
-      top: "40px",
-      left: "40px",
-      right: "40px",
-      bottom: "40px",
-      border: "1px solid #ccc",
-      background: "#fff",
-      overflow: "auto",
-      WebkitOverflowScrolling: "touch",
-      borderRadius: "4px",
-      outline: "none",
-      padding: "20px",
-      width: "450px",
-      height: "250px",
-      margin: "0 auto",
-    },
-  };
-
   render() {
-    let { modalIsOpen } = this.state;
     return (
       <div>
-        {modalIsOpen ? (
-          <Modal
-            modalAction={this.modalClose}
-            modalIsOpen={true}
-            modalStyle={this.massageBoxStyle}
-            render={
-              <MessageBox message="start date and end date should not be out od the range" />
-            }
-          />
-        ) : (
-          ""
-        )}
-
-        <div className="ui container">
-          <div className="flex-nav">
-            <div className="flex-nav">
-              <Form>
-                <div className="flex-nav filter-inputs-extra-margin">
-                  <Form.Group>
-                    <Form.Field className=" text-align-center">
-                      <label>Start Date</label>
-                      <DatePicker
-                        // selected="2020/7/7"
-                        selectsStart
-                        isClearable
-                        showYearDropdown
-                        placeholderText="Start Date"
-                      />
-                    </Form.Field>
-
-                    <Form.Field className=" text-align-center">
-                      <label>End Date</label>
-                      <DatePicker
-                        // selected="2020/8/8"
-                        selectsStart
-                        isClearable
-                        showYearDropdown
-                        placeholderText="End Date"
-                      />
-                    </Form.Field>
-                  </Form.Group>
-                </div>
-              </Form>
-            </div>
-            <div className="flex-nav">
-              <Form.Field className="filter-inputs-extra-margin">
-                <Checkbox
-                  className="filter-inputs-font-style"
-                  toggle
-                  label="Upcomming Launches"
-                />
-              </Form.Field>
-              <Form.Field className="filter-inputs-extra-margin">
-                <Dropdown
-                  className="filter-inputs-font-style"
-                  icon="filter"
-                  text="All Launches"
-                >
-                  <Dropdown.Menu>
-                    <Dropdown.Menu scrolling>
-                      {tagOptions.map((option) => (
-                        <Dropdown.Item key={option.value} {...option} />
-                      ))}
-                    </Dropdown.Menu>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Form.Field>
-            </div>
-          </div>
-          {/* </Menu> */}
-        </div>
-
-        <div className="search-lable-container">
-          <Header as="h4">Current Filters</Header>
-          <Label.Group>
-            <Label as="a" color="orange">
-              Start-date 2014/02/08
-            </Label>
-            <Label as="a" color="olive">
-              End-date 2014/02/08
-            </Label>
-            <Label as="a" color="green">
-              Successfull
-            </Label>
-            <Label as="a" color="red">
-              Failed
-            </Label>
-            <Label as="a" color="yellow">
-              Upcomming
-            </Label>
-          </Label.Group>
-        </div>
+        <FilterInput />
 
         <LaunchList />
-
         <div className="text-align-center pagination-component-wrapper ">
           <Pagination
             activePage={4}
---
+++ src/component/FilterInput.jsx
@@ -0,0 +1,174 @@
+import React, { Component } from "react";
+
+import MessageBox from "./MessageBox";
+import Modal from "./Modal";
+import DatePicker from "react-datepicker";
+
+import { Checkbox, Dropdown, Form, Header, Label } from "semantic-ui-react";
+
+const tagOptions = [
+  {
+    key: "All Launches",
+    text: "All ",
+    value: "All",
+    label: { color: "blue", empty: true, circular: true },
+  },
+  {
+    key: "Successfull",
+    text: "Successfull",
+    value: "Successfull",
+    label: { color: "green", empty: true, circular: true },
+  },
+  {
+    key: "Failed",
+    text: "Failed",
+    value: "Failed",
+    label: { color: "red", empty: true, circular: true },
+  },
+];
+class FilterInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false,
+    };
+  }
+  modalClose = () => {
+    this.setState({ modalIsOpen: false });
+  };
+  massageBoxStyle = {
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(255, 255, 255, 0.75)",
+      zIndex: 1000,
+    },
+    content: {
+      position: "absolute",
+      top: "40px",
+      left: "40px",
+      right: "40px",
+      bottom: "40px",
+      border: "1px solid #ccc",
+      background: "#fff",
+      overflow: "auto",
+      WebkitOverflowScrolling: "touch",
+      borderRadius: "4px",
+      outline: "none",
+      padding: "20px",
+      width: "450px",
+      height: "250px",
+      margin: "0 auto",
+    },
+  };
+
+  render() {
+    let { modalIsOpen } = this.state;
+    return (
+      <div>
+        {" "}
+        {modalIsOpen ? (
+          <Modal
+            modalAction={this.modalClose}
+            modalIsOpen={true}
+            modalStyle={this.massageBoxStyle}
+            render={
+              <MessageBox message="start date and end date should not be out od the range" />
+            }
+          />
+        ) : (
+          ""
+        )}
+        <div className="ui container">
+          <div className="flex-nav">
+            <div className="flex-nav">
+              <Form>
+                <div className="flex-nav filter-inputs-extra-margin">
+                  <Form.Group>
+                    <Form.Field className=" text-align-center">
+                      <label>Start Date</label>
+                      <DatePicker
+                        // selected="2020/7/7"
+                        selectsStart
+                        isClearable
+                        showYearDropdown
+                        placeholderText="Start Date"
+                      />
+                    </Form.Field>
+
+                    <Form.Field className=" text-align-center">
+                      <label>End Date</label>
+                      <DatePicker
+                        // selected="2020/8/8"
+                        selectsStart
+                        isClearable
+                        showYearDropdown
+                        placeholderText="End Date"
+                      />
+                    </Form.Field>
+                  </Form.Group>
+                </div>
+              </Form>
+            </div>
+            <div className="flex-nav">
+              <Form.Field className="filter-inputs-extra-margin">
+                <Checkbox
+                  className="filter-inputs-font-style"
+                  toggle
+                  label="Upcomming Launches"
+                />
+              </Form.Field>
+              <Form.Field className="filter-inputs-extra-margin">
+                <Dropdown
+                  className="filter-inputs-font-style"
+                  icon="filter"
+                  text="All Launches"
+                >
+                  <Dropdown.Menu>
+                    <Dropdown.Menu scrolling>
+                      {tagOptions.map((option) => (
+                        <Dropdown.Item key={option.value} {...option} />
+                      ))}
+                    </Dropdown.Menu>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Form.Field>
+            </div>
+          </div>
+        </div>
+        <div className="search-lable-container">
+          <Header
+            as="h4"
+            onClick={() => {
+              this.setState({ modalIsOpen: true });
+            }}
+          >
+            Current Filters
+          </Header>
+          <Label.Group>
+            <Label as="a" color="orange">
+              Start-date 2014/02/08
+            </Label>
+            <Label as="a" color="olive">
+              End-date 2014/02/08
+            </Label>
+            <Label as="a" color="green">
+              Successfull
+            </Label>
+            <Label as="a" color="red">
+              Failed
+            </Label>
+            <Label as="a" color="yellow">
+              Upcomming
+            </Label>
+          </Label.Group>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FilterInput;
